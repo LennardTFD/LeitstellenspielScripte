bab35f01c411ddebc7b1df01959269b189ed364e
Lennard Giesing <lennard.giesing@gmail.com>
2019-09-17 18:39:19 +0200

    addded missionmarkerglow

---
+++ LSS_MissionMarkerGlow/README.md
@@ -0,0 +1,6 @@
+<h1>LSS Alliance Building Hider</h1>
+Dieses Script entfernt alle dem Verband gehörenden Gebäude Marker von der Karte
+
+<hr>
+
+<h6>Diese Idee stammt von <a href="https://forum.leitstellenspiel.de/cms/index.php?user/8497-locke/">Locke</a></h6>
---
+++ LSS_MissionMarkerGlow/missionMarkerGlow.user.js
@@ -0,0 +1,58 @@
+// ==UserScript==
+// @name         Mission Marker Glow
+// @namespace    https://leitstellenspiel.de
+// @version      1.2
+// @description  Hides buildings of alliance
+// @author       Lennard[TFD]
+// @match        https://www.leitstellenspiel.de/
+// @match        https://www.missionchief.com/
+// @match        https://www.meldkamerspel.com/
+// @updateURL    https://github.com/LennardTFD/LeitstellenspielScripte/raw/master/LSS_AllianceBuildingHider/allianceBuildingHider.user.js
+// @downloadURL  https://github.com/LennardTFD/LeitstellenspielScripte/raw/master/LSS_AllianceBuildingHider/allianceBuildingHider.user.js
+// @grant        none
+// ==/UserScript==
+
+//Determine if MapKit or Leaflet is used
+var mapType;
+if('undefined' == typeof mapkit){
+    mapType = "leaflet";
+}
+else
+{
+    mapType = "mapkit";
+}
+
+function markerListener()
+{
+    if(mapType == "leaflet")
+    {
+        map.on('moveend', function(e) {
+            removeAlliance();
+        });
+    }
+    else
+    {
+        map.addEventListener("region-change-end", function(e) {
+            removeAlliance();
+        });
+    }
+}
+function removeAlliance()
+{
+    var listOfAllianceBuildings = [];
+    building_markers_cache.forEach((e) => {
+        if(e.user_id == null){
+            listOfAllianceBuildings.push(e["id"]);
+        }
+    });
+    $.each(building_markers, function (e, t) {
+        if(listOfAllianceBuildings.indexOf(t["building_id"]) != -1)
+        {
+            'undefined' == typeof mapkit ? map.removeLayer(t)  : map.removeAnnotation(t);
+        }
+    });
+}
+(function() {
+    'use strict';
+    markerListener();
+})();

217c0a086a4a21728830d111173788b5d9374797
Lennard[TFD] <userman2002lp@gmail.com>
2019-09-24 17:44:56 +0200

    Marker Glow Fix for default markers

--- LSS_MissionMarkerGlow/missionMarkerGlowPermanent.user.js
+++ LSS_MissionMarkerGlow/missionMarkerGlowPermanent.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Mission Marker Glow
 // @namespace    https://leitstellenspiel.de
-// @version      1.0
+// @version      1.1
 // @description  Adds glow to mission marker
 // @author       Lennard[TFD]
 // @match        https://www.leitstellenspiel.de/
@@ -19,5 +19,8 @@
         ".leaflet-marker-icon[src*='red']{ filter: drop-shadow(0px 0px 8px red)}" +
         ".leaflet-marker-icon[src*='yellow']{ filter: drop-shadow(0px 0px 8px yellow)} " +
         ".leaflet-marker-icon[src*='green']{ filter: drop-shadow(0px 0px 8px green)} " +
+        ".leaflet-marker-icon[src*='rot']{ filter: drop-shadow(0px 0px 8px red)}" +
+        ".leaflet-marker-icon[src*='gelb']{ filter: drop-shadow(0px 0px 8px yellow)} " +
+        ".leaflet-marker-icon[src*='gruen']{ filter: drop-shadow(0px 0px 8px green)} " +
         "</style>").appendTo("head");
 })();
